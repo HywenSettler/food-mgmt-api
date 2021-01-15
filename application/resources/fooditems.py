9db5df143dc8b1040356c929c77bce62b7752033
Kristency <nandisubhajit666@gmail.com>
2021-01-15 06:22:26 +0530

    fix: unique fooditems

--- application/resources/fooditems.py
+++ application/resources/fooditems.py
@@ -16,15 +16,19 @@ class FoodItems(Resource):
         search_query = request.args.get('searchQuery')
         # select * from menuitems where type=item_type and name ilike "%{searchQuery}%"
         if not item_type:
-            found_items = db.session.query(
-                MenuItem.name.distinct()).filter(
-                MenuItem.name.ilike(f"%{search_query}%")).all()
+            found_items = MenuItem.query.filter(MenuItem.name.ilike(f"%{search_query}%")).all()
+            uniq_food_items = {}
+            for food_item in found_items:
+                if food_item.name not in uniq_food_items:
+                    uniq_food_items[food_item.name] = food_item.to_json()
+
+            return list(uniq_food_items.values()), 200
         else:
             found_items = MenuItem.query.filter_by(
                 type=mapping.get(item_type)).filter(
                 MenuItem.name.ilike(f"%{search_query}%")).all()
 
-        return [item.to_json() for item in found_items], 200
+            return [item.to_json() for item in found_items], 200
 
     def post(self):
         data = request.get_json()
